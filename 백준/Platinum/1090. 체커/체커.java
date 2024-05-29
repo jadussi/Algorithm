e1ecfaa577f203ed3494c46134da6bb20d7d4183
jadussi <136336510+jadussi@users.noreply.github.com>
2024-05-29 23:33:34 +0900

    [Platinum IV] Title: 체커, Time: 212 ms, Memory: 22752 KB -BaekjoonHub

--- 백준/Platinum/1090. 체커/체커.java
+++ 백준/Platinum/1090. 체커/체커.java
@@ -1,56 +1,65 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int cnt = Integer.parseInt(br.readLine());
+        
+        // 두개 좌 표
+        int[][] xy = new int[cnt][2];
+        
+        // 왼쪽, 오른쪽 숫자
+        int[] l = new int[cnt];
+        int[] r = new int[cnt];
+        
+        int[] ans = new int[cnt];
+        // 배열 초기값 설정
+        Arrays.fill(ans, Integer.MAX_VALUE);
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int N = scanner.nextInt();
-        int[][] checkers = new int[N][2];
-
-        for (int i = 0; i < N; i++) {
-            checkers[i][0] = scanner.nextInt();
-            checkers[i][1] = scanner.nextInt();
+        // 값을 입력받기
+        for(int i = 0; i < cnt; i++) {
+            String[] st = br.readLine().split(" ");
+            int templ = Integer.parseInt(st[0]);
+            int tempr = Integer.parseInt(st[1]);
+            l[i] = templ;
+            r[i] = tempr;
+            xy[i][0] = templ;
+            xy[i][1] = tempr;
         }
 
-        int[] result = minMovesToGatherCheckers(N, checkers);
+        // 최소거리 계산
+        // 이중 반복문을 통해 비교하고자 하는 지점 선택
+        for(int i = 0; i < l.length; i++) {
+            for(int j = 0; j < r.length; j++) {
 
-        for (int moves : result) {
-            System.out.print(moves + " ");
-        }
-    }
+                // 선택한 지점에서 입력 받은 각 지점에 대한 거리 계산
+                int[] temp = new int[cnt];
+                for(int k = 0; k < cnt; k++) {
+                    temp[k] = Math.abs(l[i]-xy[k][0]) + Math.abs(r[j]-xy[k][1]);
+                }
 
-    public static int[] minMovesToGatherCheckers(int N, int[][] checkers) {
-        int[] results = new int[N];
+                // 현재 지점에 대해서 가장 가까이에 있는 거리 계산
+                // 정렬을 통해 계산한다.
+                Arrays.sort(temp);
 
-        for (int k = 1; k <= N; k++) {
-            int minMoves = Integer.MAX_VALUE;
-
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    int cx = checkers[i][0];
-                    int cy = checkers[j][1];
-                    int[] distances = new int[N];
-
-                    for (int l = 0; l < N; l++) {
-                        distances[l] = Math.abs(cx - checkers[l][0]) + Math.abs(cy - checkers[l][1]);
-                    }
-
-                    Arrays.sort(distances);
-
-                    int sum = 0;
-                    for (int l = 0; l < k; l++) {
-                        sum += distances[l];
-                    }
-
-                    minMoves = Math.min(minMoves, sum);
+                // 만나야 하는 점의 수만큼 최소 이동을 하는 경우 정답에 대입
+                int sum = 0;
+                for(int k = 0; k < cnt; k++) {
+                    sum += temp[k];
+                    ans[k] = Math.min(ans[k], sum);
                 }
             }
-
-            results[k - 1] = minMoves;
         }
 
-        return results;
-    }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < ans.length;  i++) {
+            sb.append(ans[i] + " ");
+        }
 
+        System.out.println(sb.toString());
+        
+    }
 }
