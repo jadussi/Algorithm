a4ee8f82d6827cfaa9970191797f22e58e87e86b
jadussi <136336510+jadussi@users.noreply.github.com>
2024-06-17 23:19:13 +0900

    [Gold IV] Title: 2로 몇 번 나누어질까, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.java
+++ 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.java
@@ -3,31 +3,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] str = br.readLine().split(" ");
-        long st = Long.parseLong(str[0]);
+        long st = Long.parseLong(str[0]) - 1; // 여기에서 바로 -1 처리
         long end = Long.parseLong(str[1]);
+
+        // 초기값 설정
+        long stSum = 0;
+        long endSum = 0;
         
-        long endSum = calculateSum(end);
-        long stSum = calculateSum(st - 1);
-        
-        System.out.println(endSum - stSum);
-    }
-    
-    private static long calculateSum(long num) {
-        long sum = 0;
+        // endSum 계산
         long i = 2;
         long j = 1;
-        
-        while (i <= num) {
-            sum += (num / i) * j;
+        while (i <= end) {
+            endSum += (end / i) * j;
             i *= 2;
             j *= 2;
         }
-        
-        sum += num;
-        return sum;
+        endSum += end;
+
+        // stSum 계산
+        i = 2;
+        j = 1;
+        while (i <= st) {
+            stSum += (st / i) * j;
+            i *= 2;
+            j *= 2;
+        }
+        stSum += st;
+
+        // 결과 출력
+        System.out.println(endSum - stSum);
     }
 }
