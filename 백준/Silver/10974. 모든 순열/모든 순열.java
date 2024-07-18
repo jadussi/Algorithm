5b49bdbad781144a7d3a5d90385c062a41c84825
jadussi <136336510+jadussi@users.noreply.github.com>
2024-07-18 17:20:12 +0900

    [Silver III] Title: 모든 순열, Time: 2820 ms, Memory: 74532 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static boolean[] tf ;
+    static ArrayList<Integer> arr;
+    static int n;
+    static int[] ll;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(new InputStreamReader(System.in));
+        n = scan.nextInt();
+
+        ll = new int[n];
+
+        for(int i = 1; i <=n; i++) {
+            ll[i-1] = i;
+        }
+
+        tf = new boolean[n];
+
+        arr = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            tf[i] = true;
+            arr.add(ll[i]);
+            func();
+            tf[i] = false;
+        }
+    }
+
+    static void func() {
+        if(arr.size() == n) {
+            for(int i : arr) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+            arr.remove(n-1);    // 마지막 요소 제거
+            return;
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(!tf[i]) {
+                tf[i] = true;
+                arr.add(ll[i]);
+                func();
+                tf[i] = false;
+            }
+        }
+        arr.remove(arr.size()-1);
+    }
+}
