21447bee48d0cba10cf8745bac6c25beece960b7
jadussi <136336510+jadussi@users.noreply.github.com>
2024-07-22 13:55:46 +0900

    [Silver III] Title: 2×n 타일링, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,28 +1,24 @@
-import java.util.Scanner;
-
+import java.io.*;
+import java.util.*;
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // n 입력 받기
-        int n = scanner.nextInt();
-        
-        // dp 배열 초기화
-        int[] dp = new int[n + 1];
-        
-        // 초기값 설정
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        int[] dp = new int[n+1];
+
         dp[1] = 1;
-        if (n >= 2) dp[2] = 2;
-        
-        // 동적 프로그래밍 수행
-        for (int i = 3; i <= n; i++) {
-            // 현재 위치에 오는 방법은 이전 위치에서 1칸을 올라오는 경우와 2칸을 올라오는 경우의 수를 합한 것이다.
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        if(n >= 2) {
+            dp[2] = 2;
         }
-        
-        // 결과 출력
+
+        for(int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2] ) % 10007;
+        }
+
         System.out.println(dp[n]);
-        
-        scanner.close();
+
     }
 }
